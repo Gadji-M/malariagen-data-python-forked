fe39a174eb3259e5006563f208e62aa4d87a9e3a
Gadji-M <gadji.mahamat@crid-cam.net>
2026-07-26 02:33:40 +0100

    Reverting and sending to another branch

--- malariagen_data/anopheles.py
+++ malariagen_data/anopheles.py
@@ -48,7 +48,6 @@ from .anoph.describe import AnophelesDescribe
 from .anoph.dipclust import AnophelesDipClustAnalysis
 from .anoph.heterozygosity import AnophelesHetAnalysis
 from .anoph.xpehh import AnophelesXpehhAnalysis
-from .anoph.admixture import AnophelesAdmixtureAnalysis
 from .util import (
     CacheMiss,
     Region,  # noqa: F401 (re-exported via __init__.py)
@@ -80,7 +79,6 @@ from .util import (
 # work around pycharm failing to recognise that doc() is callable
 # noinspection PyCallingNonCallable
 class AnophelesDataResource(
-    AnophelesAdmixtureAnalysis,  # Newly implemented as part of the technical test
     AnophelesDipClustAnalysis,
     AnophelesHapClustAnalysis,
     AnophelesXpehhAnalysis,
